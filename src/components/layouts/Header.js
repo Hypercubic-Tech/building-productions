7a9ca4bcaa626a8282b4126b6af8df41301003d8
David Tavadze <tavadzed@gmail.com>
2023-04-17 14:57:54 +0400

    last

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -2,7 +2,7 @@ import DefaultHeader from "./DefaultHeader";
 import HeaderLogged from "./HeaderLogged";
 
 function Header({ loggedIn }) {
-  return <>{loggedIn ? <DefaultHeader  /> : <HeaderLogged />}</>;
+  return <>{loggedIn ? <HeaderLogged /> : <DefaultHeader  />}</>;
   // return <HeaderLogged />;
 }
 
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,4 +1,4 @@
-import ListItem from "./projedtComps/ListItem";
+import ListItem from "./components/ListItem";
 
 const Projects = () => {
   return (
