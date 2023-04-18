ac670ebaa6fc5a7fa9e2b99399048c42b919b60d
keksik95 <geliashvilidato95@gmail.com>
2023-04-18 13:09:01 +0400

    request is working

--- src/components/popup/RegModal.js
+++ src/components/popup/RegModal.js
@@ -27,6 +27,7 @@ function RegModal(props) {
     if (!fullName.value || !email.value || !password.value) {
     } else {
       try {
+        console.log(email.value, password.value, fullName.value, "test");
         const response = await axios.post("/api/register", {
           email: email.value,
           password: password.value,
