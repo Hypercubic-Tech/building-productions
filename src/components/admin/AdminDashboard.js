c868673924e0cd6daf2538e96b4775dab246637f
nikak01 <eqiza99@gmail.com>
2023-04-24 14:59:00 +0400

    update

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -1,24 +1,88 @@
-import React, { useState } from 'react';
-import axiosInstance from '@/api/axios';
+import React, { useState } from "react";
+import axiosInstance from "@/api/axios";
 
 const AdminDashboard = () => {
-    const [ categoryName, setCategoryName ] = useState('');
+  const [categoryName, setCategoryName] = useState("");
+  const [craftData, setCraftData] = useState({
+    image: null,
+    craftName: "",
+  });
 
-    const addCategoryHandler = () => {
-        axiosInstance.post('/api/admin/content/add_category', {
-            category: categoryName
-        })
-        .then(res => {
-            console.log(res);
-        });
-    };
+  console.log
 
-    return (
-        <>
-            <input onChange={e => setCategoryName(e.target.value)} type="text" name="category" placeholder="Enter category" />
-            <div onClick={addCategoryHandler}>Add</div>
-        </>
-    );
+ 
+
+  const addCategoryHandler = () => {
+    axiosInstance
+      .post("/api/admin/content/add_category", {
+        category: categoryName,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  const submitHandler = () => {
+    const formData = new FormData();
+    formData.append("image", craftData.image);
+    formData.append("craftName", craftData.craftName);
+
+
+    axiosInstance
+      .post(
+        "/api/admin/content/add_craft", formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          onChange={(e) => setCategoryName(e.target.value)}
+          type="text"
+          name="category"
+          placeholder="Enter category"
+        />
+        <div onClick={addCategoryHandler}>Add</div>
+      </div>
+      <div>
+        <form>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => {
+              setCraftData((prevCraftData) => ({
+                ...prevCraftData,
+                image: e.target.files[0],
+              }));
+            }}
+          />
+          <input
+            type="text"
+            placeholder="add craft"
+            onChange={(e) => {
+              setCraftData((prevCraftData) => ({
+                ...prevCraftData,
+                craftName: e.target.value,
+              }));
+            }}
+          />
+          <div onClick={submitHandler}>add craft</div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
