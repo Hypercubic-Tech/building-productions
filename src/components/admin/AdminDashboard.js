d3696b4a1ef365dd10d6575c720efee0feca626e
nikak01 <eqiza99@gmail.com>
2023-04-26 13:13:01 +0400

    update

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -4,10 +4,12 @@ import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 
 const AdminDashboard = () => {
   const [categoryName, setCategoryName] = useState("");
+  const [categories, setCategories] = useState(null);
   const [crafts, setCrafts] = useState(null);
   const [craftData, setCraftData] = useState({
     image: null,
     craftName: "",
+    category: "",
   });
 
   const axiosPrivate = useAxiosPrivate();
@@ -26,7 +28,7 @@ const AdminDashboard = () => {
     const formData = new FormData();
     formData.append("image", craftData.image);
     formData.append("craftName", craftData.craftName);
-
+    formData.append("category", craftData.category)
     axiosInstance
       .post("/api/admin/content/add_craft", formData, {
         headers: {
@@ -41,13 +43,28 @@ const AdminDashboard = () => {
       });
   };
 
+  // useEffect(() => {
+  //   const getDataHandler = async () => {
+  //     await axiosPrivate
+  //       .get("/api/admin/content/get_crafts", {})
+  //       .then((res) => {
+  //         let data = res.data;
+  //         setCrafts(data);
+  //       })
+  //       .catch((e) => {
+  //         console.log(e, "error");
+  //       });
+  //   };
+  //   getDataHandler();
+  // }, []);
+
   useEffect(() => {
     const getDataHandler = async () => {
       await axiosPrivate
-        .get("/api/admin/content/get_crafts", {})
+        .get("/api/admin/content/get_categories", {})
         .then((res) => {
           let data = res.data;
-          setCrafts(data);
+          setCategories(data);
         })
         .catch((e) => {
           console.log(e, "error");
@@ -56,7 +73,6 @@ const AdminDashboard = () => {
     getDataHandler();
   }, []);
 
-  console.log(crafts);
   return (
     <div>
       <div>
@@ -68,8 +84,10 @@ const AdminDashboard = () => {
         />
         <div onClick={addCategoryHandler}>Add</div>
       </div>
-      <div>
-        <form>
+      <div style={{padding: '20px', marginTop: '100px', border: '1px solid black'}}>
+        <form
+          style={{ display: "flex", width: "50%", flexDirection: "column", gap: "30px" }}
+        >
           <input
             type="file"
             accept="image/*"
@@ -90,17 +108,35 @@ const AdminDashboard = () => {
               }));
             }}
           />
+          <select
+            onChange={(event) => {
+              setCraftData((prevCraftData) => ({
+                ...prevCraftData,
+                category: event.target.value,
+              }));
+            }}
+            className={`${"form-select"} ${"form-select-solid"} ${"georgian"}`}
+          >
+            {categories &&
+              categories.map((item, index) => {
+                return (
+                  <option key={index} value={item.category}>
+                    {item.category}
+                  </option>
+                );
+              })}
+          </select>
           <div onClick={addCraftHandler}>add craft</div>
         </form>
       </div>
-      {crafts?.map((item, index) => {
+      {/* {crafts?.map((item, index) => {
         return (
           <div key={index}>
             <img src={item.publicUrl} />
             <p>{item.craftName}</p>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
