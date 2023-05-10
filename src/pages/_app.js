8f0527165171242f9da1e3790e5352b24b1b4540
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-05-10 15:09:26 +0400

    updated persistor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,16 @@
 import "@/styles/main/style.bundle.css";
 import { Provider } from "react-redux";
-import { wrapper } from "../store/store";
 import "../../public/assets/plugins/global/plugins.bundle.css";
 import Layout from "../components/layout/Layout";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "../store/store"
+import store, { persistor } from "../store/store"
 export default function App({ Component, ...rest }) {
-  const { store, props } = wrapper.useWrappedStore(rest);
-  const { pageProps } = props;
 
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Layout>
-          <Component {...pageProps} />
+          <Component {...rest} />
         </Layout>
       </PersistGate>
     </Provider>
--- src/store/store.js
+++ src/store/store.js
@@ -7,11 +7,6 @@ import headerPopUpReducer from "./slices/headerModalSlice";
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const persistConfig = {
-  key: 'root',
-  storage
-};
-
 const rootReducer = combineReducers({
   [authSlice.name]: authSlice.reducer,
   modal: modalReducer,
@@ -19,18 +14,21 @@ const rootReducer = combineReducers({
   [projectSlice.name]: projectSlice.reducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const makeStore = () =>
-  configureStore({
-    reducer: persistedReducer,
+const store = configureStore({
+  reducer: persistedReducer,
     devTools: true,
     middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-  });
+})
+export default store;
 
-export const wrapper = createWrapper(makeStore);
-
-export const persistor = persistStore(makeStore());
+export const persistor = persistStore(store);
