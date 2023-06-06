a5eafa084c68f2fb2218631285c3e2316c4da1d7
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-06 18:40:49 +0400

    fixed registration tabs

--- src/components/popup/RegModal.js
+++ src/components/popup/RegModal.js
@@ -7,8 +7,6 @@ import styles from "../popup/RegModal.module.css";
 const RegModal = ({ handleRegistration, onClose }) => {
   const [step, setStep] = useState(1);
   const [lossData, setLossData] = useState(false);
-  const [showSecondTab, setShowSecondTab] = useState(false);
-  const [showThirdTab, setShowThirdTab] = useState(false);
   const [paymentPlan, setPaymentPlan] = useState(null);
   const [regData, setRegData] = useState({
     username: "",
@@ -27,16 +25,17 @@ const RegModal = ({ handleRegistration, onClose }) => {
       regData.phoneNumber.length > 0 &&
       regData.password.length > 0
     ) {
-      setShowSecondTab(true);
+      setStep(1)
       setLossData(false);
     } else {
+      setStep(2)
       setLossData(true);
     }
   };
 
   const stepChangeHandler2 = () => {
     if (paymentPlan === "paid") {
-      setShowThirdTab(true);
+      setStep(3)
       console.log(showThirdTab, 'showThirdTab')
     }
   };
@@ -65,9 +64,9 @@ const RegModal = ({ handleRegistration, onClose }) => {
   };
 
   return (
-    <div className={`${getStatusClass(showSecondTab ? 1 : 2)} ${styles.container}`} >
+    <div className={`${getStatusClass(step)} ${styles.container}`} >
       <form onSubmit={handleSubmit}>
-        {showSecondTab ? (
+        {step == 2 ? (
           <div className={getStatusClass(2)}>
             <div className="col">
               <div className="d-flex justify-content-between align-items-center mb-2">
@@ -118,7 +117,7 @@ const RegModal = ({ handleRegistration, onClose }) => {
                       setPaymentPlan(e.target.value)
                     }}
                   >
-                    <option disabled value="აირჩიეთ გადახდის მეთოდი">აირჩიეთ გადახდის გეგმა</option>
+                    <option disabled defaultValue="აირჩიეთ გადახდის გეგმა">აირჩიეთ გადახდის გეგმა</option>
                     <option id="1" value="free">უფასო</option>
                     <option id="2" value="paid">ფასიანი</option>
                   </select>
@@ -126,7 +125,7 @@ const RegModal = ({ handleRegistration, onClose }) => {
                 <button
                   className={` btn btn-success georgian ${styles.btn}`}
                   type={regData.paymentMethod === "free" ? 'submit' : 'button'}
-                  onClick={stepChangeHandler2}
+                  onClick={() => regData.paymentMethod === "paid" ? setStep(3) : ""}
 
                 >
                   {regData.paymentMethod === "free" ? 'რეგისტრაცია' : 'შემდეგ'}
@@ -134,71 +133,9 @@ const RegModal = ({ handleRegistration, onClose }) => {
               </div>
             </div>
           </div>
-        ) : showThirdTab ? (
-          <div className={getStatusClass(3)}>
-            <div className="col">
-              <div className="d-flex justify-content-between align-items-center mb-2">
-                <div className="text-muted">რეგისტრაცია</div>
-                <svg
-                  onClick={onClose}
-                  className={`${styles.closeBtn}`}
-                  width="64px"
-                  height="64px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    stroke="#CCCCCC"
-                    strokeWidth="0.336"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <g id="Menu / Close_MD">
-                      <path
-                        id="Vector"
-                        d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
-                        stroke="#000000"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      ></path>
-                    </g>
-                  </g>
-                </svg>
-              </div>
-              <div className="d-grid gap-2 mt-n1">
-                <div className="d-grid gap-2 mt-n1">
-                  <label className="mt-2">აირჩიეთ გადახდის მეთოდი:</label>
-                  <select
-                    className="form-select form-select-solid georgian"
-                    defaultValue="აირჩიეთ გადახდის გეგმა"
-                    onChange={(e) => {
-                      setRegData((prevSendData) => ({
-                        ...prevSendData,
-                        paymentMethod: e.target.value
-                      }));
-                    }}
-                  >
-                    <option disabled value="აირჩიეთ გადახდის მეთოდი">აირჩიეთ გადახდის მეთოდი</option>
-                    <option id="1" value="tbc">TBC</option>
-                    <option id="2" value="bog">BOG</option>
-                  </select>
-                </div>
-                <button
-                  className={` btn btn-success georgian ${styles.btn}`}
-                  type="button"
-                  onClick={stepChangeHandler2}
-                >
-                  შემდეგ
-                </button>
-              </div>
-            </div>
-          </div>
-        ) : (
+        ) : ""}
+        {step === 1 ? (
+
           <div className="col">
             <div className="d-flex justify-content-between align-items-center mb-2">
               <div className="text-muted">რეგისტრაცია</div>
@@ -354,13 +291,78 @@ const RegModal = ({ handleRegistration, onClose }) => {
               <button
                 className={` btn btn-success georgian ${styles.btn}`}
                 type="button"
-                onClick={stepChangeHandler}
+                onClick={() => setStep(2)}
               >
                 შემდეგ
               </button>
             </div>
           </div>
-        )}
+        ) : ""}
+        {step === 3 ? (
+          <div className={getStatusClass(3)}>
+            <div className="col">
+              <div className="d-flex justify-content-between align-items-center mb-2">
+                <div className="text-muted">რეგისტრაცია</div>
+                <svg
+                  onClick={onClose}
+                  className={`${styles.closeBtn}`}
+                  width="64px"
+                  height="64px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    stroke="#CCCCCC"
+                    strokeWidth="0.336"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <g id="Menu / Close_MD">
+                      <path
+                        id="Vector"
+                        d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+                        stroke="#000000"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              <div className="d-grid gap-2 mt-n1">
+                <div className="d-grid gap-2 mt-n1">
+                  <label className="mt-2">აირჩიეთ გადახდის მეთოდი:</label>
+                  <select
+                    className="form-select form-select-solid georgian"
+                    defaultValue="აირჩიეთ გადახდის გეგმა"
+                    onChange={(e) => {
+                      setRegData((prevSendData) => ({
+                        ...prevSendData,
+                        paymentMethod: e.target.value
+                      }));
+                    }}
+                  >
+                    <option disabled value="აირჩიეთ გადახდის მეთოდი">აირჩიეთ გადახდის მეთოდი</option>
+                    <option id="1" value="tbc">TBC</option>
+                    <option id="2" value="bog">BOG</option>
+                  </select>
+                </div>
+                <button
+                  className={` btn btn-success georgian ${styles.btn}`}
+                  type="button"
+                  onClick={stepChangeHandler2}
+                >
+                  შემდეგ
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : ""}
       </form>
     </div>
   );
