7849b85dc96785196226f730a7e407974f588b41
keksik95 <geliashvilidato95@gmail.com>
2023-06-14 16:36:42 +0400

    edit styles

--- src/components/popup/AddProject.js
+++ src/components/popup/AddProject.js
@@ -413,7 +413,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                               }}
                               name="area"
                               type="number"
-                              className="form-control georgian form-control-solid"
+                              className="custom-input form-control georgian form-control-solid"
                               placeholder="ობიექტის ფართობი"
                               data-placeholder="area"
                             ></input>
@@ -437,7 +437,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                                 }));
                               }}
                               type="text"
-                              className="form-control georgian form-control-solid"
+                              className="custom-input form-control georgian form-control-solid"
                               placeholder="ზუსტი მისამართი"
                             />
                           </div>
@@ -454,7 +454,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                                 }));
                               }}
                               type="number"
-                              className="form-control georgian form-control-solid"
+                              className="custom-input form-control georgian form-control-solid"
                               placeholder="ტელეფონი"
                             />
                           </div>
@@ -489,7 +489,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                             <span className={` georgian `}>დღგ-ს გადამხდელი</span>
                           </label>
                           <input
-                            className="form-control georgian form-control-solid"
+                            className="custom-input form-control georgian form-control-solid"
                             placeholder="დღგ-ს გადამხდელი (%)"
                             type="text"
                             onChange={(e) => {
@@ -514,7 +514,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                               }));
                             }}
                             type="text"
-                            className="form-control georgian form-control-solid"
+                            className="custom-input form-control georgian form-control-solid"
                             placeholder="გაუთვალისწინებელი ხარჯები (%)"
                           />
                           {/* <i className={`${styles.percent} bi bi-percent `}></i> */}
@@ -525,7 +525,7 @@ const AddProject = ({ dismiss, setShowProject }) => {
                               <span className={` georgian `}>მომსახურეობის ხარჯები </span>
                             </label>
                             <input
-                              className="form-control georgian form-control-solid"
+                              className="custom-input form-control georgian form-control-solid"
                               type="text"
                               id="flexSwitchCheckDefault"
                               placeholder="მომსახურების ხარჯები (%)"
