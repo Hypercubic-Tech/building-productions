13b79f0fea33ab9d8f41458346e01d3cb6c035f0
whatever <eqiza99@gmail.com>
2023-06-20 15:26:25 +0400

    update

--- src/pages/401.js
+++ src/pages/401.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
+import styles from "../styles/main.module.css";
+
 const Unauthorized = () => {
     return (
-        <div>
+        <div className={styles.not_found}>
             <h1>401 - Unauthorized</h1>
             <p>Sorry, you are not authorized to access this page.</p>
-            <Link>Go to main page</Link>
+            <Link className={styles.button} href={'/'}>Go to main page</Link>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import { setAuthAccessToken, setAuthEmail, setAuthUserId } from "../store/slices/authSlice";
 import jwt_decode from "jwt-decode";
